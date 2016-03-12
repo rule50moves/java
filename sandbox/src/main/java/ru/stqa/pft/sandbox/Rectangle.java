a3f9dd17088f58503115f7939f4fb15d490607a9
rule50moves <mazurenko_stas@mail.ru>
2016-03-12 17:57:20 +0300

    Классы, объекты, методы и функции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Мазуренко on 12.03.2016.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public  Rectangle(double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area(){
+    return this.a * this.b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Мазуренко on 12.03.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area(){
+    return this.l * this.l;
+  }
+}
