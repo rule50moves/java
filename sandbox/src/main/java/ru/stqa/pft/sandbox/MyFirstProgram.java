29d964022d0d8036e45160068f76ea994a8a1b4f
rule50moves <mazurenko_stas@mail.ru>
2016-09-23 10:56:26 +0300

    1111

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,20 +4,20 @@ import java.awt.*;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Point p1 = new Point(2, 4);
-    Point p2 = new Point(8, 6);
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(8, 6);
 
-    System.out.println("Расстояние между точкой (" + p1.x + ", " + p1.y + ") и точкой (" + p2.x + ", " + p2.y + ") равно " + p1.distance(p2));
+        System.out.println("Расстояние между точкой (" + p1.x + ", " + p1.y + ") и точкой (" + p2.x + ", " + p2.y + ") равно " + p1.distance(p2));
 
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-  }
+    }
 
 }
