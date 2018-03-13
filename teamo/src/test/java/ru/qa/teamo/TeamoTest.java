9bc80f36c8160d6b537d41c04dd9a58c30a5809b
rule50moves <mazurenko_stas@mail.ru>
2018-03-13 16:52:34 +0300

    testAuthorization

---
+++ teamo/src/test/java/ru/qa/teamo/TeamoTest.java
@@ -0,0 +1,58 @@
+package ru.qa.teamo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class TeamoTest {
+    ChromeDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void testAuthorization() {
+        wd.get("https://teamo.ru/signin");
+        wd.findElement(By.id("user_email")).click();
+        wd.findElement(By.id("user_email")).clear();
+        wd.findElement(By.id("user_email")).sendKeys("rge@ya.ru");
+        wd.findElement(By.id("user_password")).click();
+        wd.findElement(By.id("user_password")).clear();
+        wd.findElement(By.id("user_password")).sendKeys("1");
+        wd.findElement(By.cssSelector("input.submit")).click();
+
+        String actualUrl = wd.getCurrentUrl();
+        String expectedUrl = "https://teamo.ru/me";
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(ChromeDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
