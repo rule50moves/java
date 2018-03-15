db4bc9a7f5f5f96cf5b5cd4805fdf178c52c83da
rule50moves <mazurenko_stas@mail.ru>
2018-03-15 17:03:44 +0300

    Раздел Лучшие

---
+++ teamo/src/test/java/ru/qa/teamo/BestTest.java
@@ -0,0 +1,56 @@
+package ru.qa.teamo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class BestTest {
+    ChromeDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void testBest() {
+        ElementsFactory elements = new ElementsFactory();
+        wd.get("https://teamo.ru/signin");
+        wd.findElement(By.id("user_email")).click();
+        wd.findElement(By.id("user_email")).clear();
+        wd.findElement(By.id("user_email")).sendKeys("qatest@teamo.ru");
+        wd.findElement(By.id("user_password")).click();
+        wd.findElement(By.id("user_password")).clear();
+        wd.findElement(By.id("user_password")).sendKeys("1");
+        wd.findElement(By.cssSelector("input.submit")).click();
+        wd.findElement(elements.getMenuItemBests()).click();
+
+        wd.findElement(By.cssSelector(".superpremium-item__info"));
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(ChromeDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ teamo/src/test/java/ru/qa/teamo/ElementsFactory.java
@@ -0,0 +1,11 @@
+package ru.qa.teamo;
+
+import org.openqa.selenium.By;
+
+public class ElementsFactory {
+
+  public By getMenuItemBests()
+  {
+    return By.linkText("Лучшие");
+  }
+}
